5d8f8b151b4efcf8c4804a153f90a24715ba7f03
bruunosanttos <bruno-vieira-santos@hotmail.com>
2024-06-22 17:22:08 -0300

    encerrando adição de funcionalidade cancelamento de consulta e fim do projeto

--- Gerador de Consultas/gerador_de _consultas.py
+++ Gerador de Consultas/gerador_de _consultas.py
@@ -1,18 +1,8 @@
 import datetime
 
-#1. Definição das Estruturas de Dados
-
-#Crie uma lista para armazenar os pacientes cadastrados.
 pacientes = []
-#Crie uma lista para armazenar as consultas agendadas.
 consultas = []
 
-
-#3. Cadastrar um Paciente
-#Peça ao usuário para inserir o nome e o telefone.
-#Verifique se o telefone já está cadastrado na lista de pacientes.
-#Se o telefone não estiver cadastrado, adicione o paciente à lista de pacientes e exiba uma mensagem de sucesso.
-#Se o telefone já estiver cadastrado, exiba uma mensagem de erro e retorne ao menu principal.
 def cadastro_paciente():
     nome = input("Digite o nome do paciente:")
     telefone = input("insira um numero de Telefone sem traços ou parenteses ex 999999999: ")
@@ -26,41 +16,33 @@ def cadastro_paciente():
     pacientes.append(novo_paciente)
     print("Paciente cadastrado com sucesso")
 
-#4. Marcar uma Consulta
-#Exiba a lista numerada de pacientes cadastrados.
 def listar_pacientes():
     for indice, paciente in enumerate(pacientes):
         print(f"{indice}: {paciente["nome"],paciente["telefone"]}")
 
-
-
-
 def marcar_consulta():
-    # Peça ao usuário para escolher um paciente a partir da lista.
     listar_pacientes()
     paciente_escolhido = int(input("Escolha um paciente para marcar consulta:"))
     confirmacao = int(input(f"O paciente escolhido foi {paciente_escolhido}? 1-Sim 2-Não"))
 
     if confirmacao == 1:
-        # Solicite a data, hora e especialidade desejada.
         data = input("Para qual data deseja marcar consulta? ex:dd-mm-aaaa: ")
         hora = input("Qual o hórario desejado?(para hórarios depois do meio dia utilizar 13:00): ")
         especialidade = input("Qual a especialidade desejada?: ")
 
-        # Verifique se a data e hora são válidas (não no passado).
         data_consulta = datetime.datetime.strptime(f"{data} {hora}", "%d-%m-%Y %H:%M")
 
         if data_consulta <= datetime.datetime.now():
-            print("Não é possivel marcar pois você marcou uma data retoativa, tente novamente...")
+            print("Não é possivel marcar pois você colocou uma data retroativa, tente novamente...")
             return
 
-        # Verifique se a data e a hora já estão ocupadas para qualquer paciente.
-        elif data_consulta == datetime.datetime.now():
-            print("ja temos consulta marcada no mesmo horário, tente outro horário.")
+        for consulta in consultas:
+            consulta_data_hora = datetime.datetime.strptime(f"{consulta['data']} {consulta['hora']}", "%d-%m-%Y %H:%M")
+            if consulta_data_hora == data_consulta:
+                print("Já temos consulta marcada no mesmo horário, tente outro horário.")
+                return
 
-
-        # Se estiverem disponíveis e válidas, adicione o agendamento à lista de consultas e exiba uma mensagem de sucesso.
-        elif data_consulta > datetime.datetime.now():
+        if data_consulta > datetime.datetime.now():
             consultas.append({
                 "paciente": paciente_escolhido,
                 "data": data,
@@ -77,38 +59,17 @@ def listar_consultas():
     for indice, consulta in enumerate(consultas):
         print(f"{indice}: {consulta["paciente"], consulta["data"], consulta["hora"],consulta["especialidade"]}")
 
-
-
-#5. Cancelar uma Consulta
 def cancelar_consulta():
-    # Exiba a lista numerada de agendamentos existentes.
     listar_consultas()
-    # Peça ao usuário para escolher um agendamento a partir da lista.
     consulta_cancelar = int(input(f"Qual consulta deseja cancelar?: "))
-    # Exiba os detalhes da consulta selecionada.
-    # Pergunte ao usuário se ele deseja cancelar a consulta.
-    confirmacao = int(input(f"A consulta {consulta_cancelar}é a que deseja cancelar?: 1-Sim 2-Não"))
-    
-    # Se sim, remova o agendamento da lista e exiba uma mensagem de confirmação
+    confirmacao = int(input(f"A consulta {consulta_cancelar} é a que deseja cancelar?: 1-Sim 2-Não"))
     if confirmacao == 1:
-        #aqui vou ter que utilizar o pop com o indice da consulta para cancelar
+        consultas.pop(consulta_cancelar)
+        print("Consulta cancelada com sucesso.")
+
 
     elif confirmacao == 2:
         print("Não foi possível cancelar a consulta, tente novamente...")
-
-
-
-
-
-
-
-#Se sim, remova o agendamento da lista e exiba uma mensagem de confirmação.
-#Se não, retorne ao menu principal.
-
-
-#2. Menu Principal
-
-#Crie um loop while que exibirá o menu principal e permitirá ao usuário escolher uma das opções:
 
 while True:
 
@@ -125,20 +86,15 @@ while True:
 Digite sua resposta:
 """))
 
-    # Cadastrar um paciente.
     if menu == 1:
         cadastro_paciente()
-    # Marcar uma consulta.
+
     elif menu == 2:
         marcar_consulta()
-    # Cancelar uma consulta.
+
     elif menu == 3:
         cancelar_consulta()
 
-    # 6. Sair
-    # Encerre o loop while e finalize o programa.
     elif menu == 4:
         print("Obrigado por utilizar nossos serviços")
         break
-
-
